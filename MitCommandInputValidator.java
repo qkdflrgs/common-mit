24370e796a58127583508d67f0d5b17468bd7650
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 17:35:10 +0900

    feat: mit 명령어 검증 객체에 exit 명령어 검증 추가

--- MitCommandInputValidator.java
+++ MitCommandInputValidator.java
@@ -1,7 +1,7 @@
 import java.util.regex.Pattern;
 
 public class MitCommandInputValidator {
-    private static final Pattern COMMAND_FORMAT = Pattern.compile("mit [list|hash|zlib]+ .+");
+    private static final Pattern COMMAND_FORMAT = Pattern.compile("(mit [list|hash|zlib]+ .+)|exit");
 
     public static boolean validate(String text){
         return COMMAND_FORMAT.matcher(text).matches();
