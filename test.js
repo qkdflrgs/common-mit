602cdb1e511f5ba997db7079c7e5bb10735336b5
Jisoo Yoo <youzysu@gmail.com>
2023-02-27 19:39:35 +0900

    test: App 클래스 메서드 테스트

---
+++ test.js
@@ -0,0 +1,5 @@
+import { App } from './App.js';
+
+const test = new App();
+test.list('/Users/youzysu/Codesquad/CS16/common-mit/testWork');
+// test.hash('/Users/youzysu/Codesquad/CS16/common-mit/testWork');
