8582e6e08cac9d462d120e4e87650c0e6b641e58
road-jin <rjswmtk@gmail.com>
2023-02-27 21:02:14 +0900

    feat: quit 입력시 프로그램이 종료되는 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
+import controller.CommandController;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        CommandController controller = new CommandController();
+        controller.run();
     }
 }
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,8 @@
+package view;
+
+public class OutputView {
+
+    public static void printQuit() {
+        System.out.println("프로그램을 종료합니다.");
+    }
+}
