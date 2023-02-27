f9a194659d2605f3fa815b92934ef00c793cdb03
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 16:49:27 +0900

    fix: 사용자 명령어 형식 버그 수정
    [list|hash|zlib]에서 [list|hash|zlib]+로 변경하여 최소 1개가 있도록 변경합니다.

--- MitCommandInputValidator.java
+++ MitCommandInputValidator.java
@@ -1,7 +1,7 @@
 import java.util.regex.Pattern;
 
 public class MitCommandInputValidator {
-    private static final Pattern COMMAND_FORMAT = Pattern.compile("mit [list|hash|zlib] .+");
+    private static final Pattern COMMAND_FORMAT = Pattern.compile("mit [list|hash|zlib]+ .+");
 
     public static boolean validate(String text){
         return COMMAND_FORMAT.matcher(text).matches();
