2e7828fc9fc7e1354b69b7c6e900e709ccabaad3
Hyejeong Park <effie.thedev@gmail.com>
2023-02-27 16:13:29 +0900

    [step0] 명령어 입력 및 결과 출력 객체 및 실행 객체 구현
    - 입출력 처리 객체 MitIOManager
    - 출력용 결과 객체 MitOutput
    - 명령어 객체 MitCommand
    - 입력 문자열을 파싱해 명령어를 만드는 static 메소드 구현
    - 간단하게 파싱 결과 출력하도록 구현

--- CS16-effie/CS16-effie/main.swift
+++ CS16-effie/CS16-effie/main.swift
@@ -6,3 +6,128 @@
 //
 
 import Foundation
+
+enum MitCommand: CustomStringConvertible {
+  case list(path: String)
+  case hash(path: String)
+  case zlib(path: String)
+  
+  var description: String {
+    switch self {
+    case .list(let p): return "list - \(p)"
+    case .hash(let p): return "hash - \(p)"
+    case .zlib(let p): return "zlib - \(p)"
+    }
+  }
+  
+  static func getCommand(from input: String) throws -> MitCommand {
+    let elems = input.components(separatedBy: .whitespaces)
+    
+    guard let maybeMit = elems.first, maybeMit == "mit" else { throw MitError.NotMit(elems[0]) }
+    
+    guard elems.count == 3 else { throw MitError.InvalidCommand }
+    
+    let command = elems[1]
+    let path = elems[2]
+    
+    switch command {
+    case "list":
+      return .list(path: path)
+    case "hash":
+      return .hash(path: path)
+    case "zlib":
+      return .zlib(path: path)
+    default:
+      throw MitError.InvalidCommand
+    }
+  }
+}
+
+enum IOError: Error {
+  case InvalidInput
+}
+
+enum MitError: Error {
+  case NotMit(_ command: String)
+  case InvalidCommand
+  case CannotFetchDirectoryInfo
+  
+  var actionMessage: String {
+    switch self {
+    case .NotMit(let command): return "command not found: \(command)"
+    case .InvalidCommand: return "존재하지 않는 mit 명령어입니다. 다시 입력해주세요."
+    case .CannotFetchDirectoryInfo: return "디렉토리를 탐색할 수 없습니다."
+    }
+  }
+}
+
+protocol MitOutput: CustomStringConvertible {
+  associatedtype InfoType
+  
+  var fileName: String { get }
+  var info: InfoType { get }
+}
+
+struct ListOutput: MitOutput {
+  typealias InfoType = Int
+  
+  var fileName: String
+  var info: Int
+  
+  var description: String {
+    "\(fileName) \(info)KB"
+  }
+}
+
+struct HashOutput: MitOutput {
+  typealias InfoType = String
+  
+  var fileName: String
+  var info: String
+  var description: String {
+    "\(fileName) = \(info)"
+  }
+}
+
+final class MitIOManager {
+  static func getInput() throws -> String {
+    print("$", terminator: " ")
+    guard let input = readLine() else { throw IOError.InvalidInput }
+    return input.trimmingCharacters(in: .whitespacesAndNewlines)
+  }
+  
+  static func printOutput(_ items: [any MitOutput]) throws {
+    items.forEach { item in print(item.description) }
+  }
+}
+
+final class Mit {
+  private var fileManager: FileManager {
+    return FileManager.default
+  }
+    
+  static func run() {
+    while true {
+      do {
+        let input = try MitIOManager.getInput()
+        guard input != "q" else {
+          print("terminated")
+          return
+        }
+        
+        let command = try MitCommand.getCommand(from: input)
+        
+        print(command) ///
+        
+      } catch {
+        if let mitError = error as? MitError {
+          print(mitError.actionMessage)
+        } else {
+          print(error)
+        }
+      }
+      
+      print("")
+    }
+  }
+}
