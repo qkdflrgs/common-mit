2b4544f82d06be5dc15836e3d3458e55488e34c1
Luka <meena2003@naver.com>
2023-02-27 17:52:46 +0900

    MitSystem 초기 클래스 골격 코드 작성

---
+++ MitSystem.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.Arrays;
+
+public class MitSystem {
+
+    File file = new File("C:\\Users\\kimdo\\CodeSquad-masters\\CS16\\example");
+
+    public void printListOfFiles() {
+        System.out.println(Arrays.toString(file.listFiles()));
+    }
+
+    public void printHashOfFiles() {
+
+    }
+
+    public void compressFiles() {
+
+    }
+
+    private void searchFiles() {
+
+    }
+}
