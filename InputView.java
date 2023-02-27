a6fd81174b8dac86f73fefd2fc4595968b66aaca
Luka <meena2003@naver.com>
2023-02-27 18:02:40 +0900

    InputView 클래스에 입력값 받는 메소드 추가
    getInput() 메소드를 생성함

--- InputView.java
+++ InputView.java
@@ -1,2 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class InputView {
+    private BufferedReader br;
+
+    public InputView() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public String getInput() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
