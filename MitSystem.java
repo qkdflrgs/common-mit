e6e1ba3113d0638d0e66a7348f39293ab70b5582
Luka <meena2003@naver.com>
2023-02-27 22:35:34 +0900

    MitSystem에 파일 리스트 출력 기능 구현
    printListOfFiles() 메소드에서 File 배열을 통해 파일인지, 디렉토리인지 확인 및 크기를 출력하는 기능 구현함.

--- MitSystem.java
+++ MitSystem.java
@@ -7,9 +7,10 @@ public class MitSystem {
     public void printListOfFiles(String directoryPath) throws NullPointerException {
         directory = new File(directoryPath);
         try {
-            String[] files = directory.list();
-            for (String file : files) {
-                System.out.println(file);
+            File[] files = directory.listFiles();
+            for (File file : files) {
+                String kind = file.isFile() ? "파일" : "디렉토리";
+                System.out.printf("<%s> %s %dKB\n", kind, file.getName(), file.length() / 1000);
             }
         } catch (NullPointerException e) {
             throw new NullPointerException("올바른 경로가 아닙니다.");
