a2e192cb777846b9ffd2b91505970103b80724b7
yeon <peach-pie@naver.com>
2023-02-27 21:59:30 +0900

    Class Mit의 hash 기능 구현 완료
    1. Class Mit에 디렉토리에서 전체 파일 목록을 탐색하고 각 파일 내용에 대한 sha256 해시 값을 출력하는 hash 기능 추가
    2. Class Application 정리

--- Mit.java
+++ Mit.java
@@ -1,7 +1,42 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Mit {
-    public void list(String path) {
+    private final InputView inputView = new InputView();
+
+    public void run() throws IOException, NoSuchAlgorithmException {
+        boolean running = true;
+        while (running) {
+            String input = inputView.getInput();
+            String[] inputs = input.split(" ");
+            String command = inputs[1];
+            String path = "";
+            if (inputs.length == 3) {
+                path = inputs[2];
+            }
+            switch (command) {
+                case "list":
+                    list(path);
+                    break;
+                case "hash":
+                    hash(path);
+                    break;
+                case "zlib":
+                    zlib(path);
+                    break;
+                case "exit":
+                    inputView.stop();
+                    running = false;
+                    break;
+            }
+        }
+    }
+
+    private void list(String path) {
         File directory = new File(path);
         File[] files = directory.listFiles();
 
@@ -15,5 +50,39 @@ public class Mit {
             long fileSize = file.length() / 1024; // kilobyte
             System.out.println(filename + " " + fileSize + "KB");
         }
+        System.out.println();
+    }
+
+    private void hash(String path) throws NoSuchAlgorithmException, IOException {
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+
+        if (files == null) {
+            try {
+                throw new FileNotFoundException("해당 경로에 파일이 존재하지 않습니다.");
+            } catch (FileNotFoundException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+
+        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        for (File file : files) {
+            digest.update(Files.readAllBytes(file.toPath()));
+            System.out.println(file.getName() + " : " + byteToHex(digest.digest()));
+        }
+        System.out.println();
+    }
+
+    private String byteToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(Integer.toHexString(0xff & b));
+        }
+        return builder.toString();
+    }
+
+    private void zlib(String path) {
+
     }
 }
