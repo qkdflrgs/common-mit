e4e7998ea7e8965c30af518240026c27e8bbd7f7
CDchan <kdjin1215@Naver.com>
2023-02-27 23:20:28 +0900

    mit의 list 기능을하는 DirectoryScanner 구현

---
+++ src/mit.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class mit {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        File directory = new File("./Work/Masters");
+
+        DirectoryScanner scanner = new DirectoryScanner(directory);
+    }
+}
