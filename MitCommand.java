9f128426c16ec1b373e69809ce2adf5b414db8c2
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-27 16:32:37 +0900

    feat: mit list 명령어 출력 함수 추가

--- MitCommand.java
+++ MitCommand.java
@@ -1,9 +1,16 @@
 import java.io.File;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public interface MitCommand {
     Optional<List<File>> list(String directoryName);
     Optional<List<String>> hash(String directoryName);
     Optional<List<String>> zlib(String directoryName);
+
+    Consumer<List<File>> outputList = (files->{
+        files.stream()
+                .map(file->String.format("%s %.2fKB", file.getName(), (double) file.length() / 1024))
+                .forEach(System.out::println);
+    });
 }
