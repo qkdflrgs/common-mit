f16b31f1f7fecf377ac28d2e40cb675b04ac826d
JaydenLee1116 <vv55adss@gmail.com>
2023-02-27 20:32:11 +0900

    feat: Add feature1 list command

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 import { InputReceiver } from './input_receiver';
+import { MitCommander } from './mit_commander';
 
 class App {
   private readonly inputReceiver: InputReceiver = new InputReceiver();
-  private readonly mitCommander: MitCommander = new MitCommander();
+  private mitCommander!: MitCommander;
   constructor() {
     (async () => {
-      const command = await this.inputReceiver.getInput();
+      const commands = (await this.inputReceiver.getInput()).split(' ');
+      const mainCommand = commands[0];
+      const subCommand = commands[1];
+      const directory = commands[2];
+
+      if (mainCommand !== 'mit')
+        throw new Error('첫번째 command가 mit가 아닙니다.');
+      this.mitCommander = new MitCommander(subCommand, directory);
     })();
   }
 }
+
+const app = new App();
