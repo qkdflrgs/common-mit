b9c703bf9c7c7a95b76cb3ac9b5a40a06885bfae
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-02-27 21:04:15 +0900

    MitCommand.java mit_hash() 추가
    파일 데이터를 sha256으로 해싱한 값을 출력하는 기능

--- MitCommand.java
+++ MitCommand.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MitCommand {
     public void controller(String input){
@@ -11,6 +15,13 @@ public class MitCommand {
         switch (command){
             case "list":
                 mit_list(dir);
+                break;
+            case "hash":
+                mit_hash(dir);
+                break;
+            case "zlib":
+
+                break;
         }
 
         System.out.println();
@@ -20,6 +31,38 @@ public class MitCommand {
         File[] files = dir.listFiles();
         for (File file : files){
             System.out.println(file.getName() + " " + String.format("%.1f", (double)file.length()/1024) + "KB");
+        }
+    }
+
+    public void mit_hash(File dir){
+        File[] files = dir.listFiles();
+        for(File file : files){
+            String hash = sha256(file);
+            if(hash.length() > 100){
+                hash = hash.substring(0, 100) + "....";
+            }
+            System.out.println(file.getName() + " = " + hash);
+        }
+    }
+
+    public String sha256(File file){
+        byte[] fileByte;
+        try {
+            fileByte = Files.readAllBytes(file.toPath());
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(fileByte);
+
+            StringBuilder sb = new StringBuilder();
+            for(byte b : fileByte){
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
     }
 }
