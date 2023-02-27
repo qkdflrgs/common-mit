e460d75f812d181cb2b0c96c4ab91866520ae467
Luka <meena2003@naver.com>
2023-02-27 22:48:00 +0900

    MintSystem 클래스의 printListOfFiles() 메소드 리팩토링
    필요없는 directory 변수 삭제함.

--- MitSystem.java
+++ MitSystem.java
@@ -2,12 +2,9 @@ import java.io.File;
 import java.util.Arrays;
 
 public class MitSystem {
-    private File directory;
-
     public void printListOfFiles(String directoryPath) throws NullPointerException {
-        directory = new File(directoryPath);
         try {
-            File[] files = directory.listFiles();
+            File[] files = new File(directoryPath).listFiles();
             for (File file : files) {
                 String kind = file.isFile() ? "파일" : "디렉토리";
                 System.out.printf("<%s> %s %dKB\n", kind, file.getName(), file.length() / 1000);
@@ -25,7 +22,4 @@ public class MitSystem {
 
     }
 
-    private void searchFiles() {
-
-    }
 }
